8d129ffc4700ccaffd9e62f134be752199c637da
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-12-11 22:57:34 -0600

    prototype of whole batch

--- test_qualimap.py
+++ test_qualimap.py
@@ -1,10 +1,19 @@
 
-
 from DNASeq import SequenceAlignmentMap, Sample
 
-jaca = Sample('JACA', '/home/rodrigo/tmp/jaca')
-
-jaca_bam = SequenceAlignmentMap( jaca )
-
-jaca_bam.link_import_sam('/home/rgarcia/tmp/file_jaca.bam')
+jaca_bam = SequenceAlignmentMap( Sample('JACA', '/home/rodrigo/tmp/jaca') )
+jaca_bam.import_map('/home/rgarcia/tmp/file_jaca.bam')
 jaca_bam.create_qualimap_lsf_job('max', 16)
+
+
+
+
+#    Id       working_path              bam_file
+samples = [
+    ('JACA', '/home/rodrigo/tmp/jaca', '/home/rgarcia/tmp/file_jaca.bam'),
+]
+
+for sample in samples:
+    bam = SequenceAlignmentMap( Sample(sample[0], sample[1]) ) )
+    bam.import_map(sample[2])
+    bam.create_qualimap_lsf_job('max', 16)
